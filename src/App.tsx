5529cd9c8a640af726f9c06ca06d2808eb99efad
cgbinho <cgbinho@gmail.com>
2021-09-23 09:47:55 -0300

    Challenge Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
@@ -41,7 +41,8 @@ export function App() {
     });
   }, []);
 
-  useEffect(() => {
+  // created a useCallback function to trigger the api call only when selectedGenreId changes:
+  const fetchMoviesAndGenres = useCallback(() => {
     api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
       setMovies(response.data);
     });
@@ -49,11 +50,16 @@ export function App() {
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
+  }, [selectedGenreId])
+
+  useEffect(() => {
+    fetchMoviesAndGenres();
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, [])
+
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
